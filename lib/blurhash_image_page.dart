1a0a70cf3d941648161decf2230830d8e7776563
Tanvir Mahtab <devtanvirtoha@gmail.com>
2023-12-18 15:47:57 +0600

    s

--- lib/blurhash_image_page.dart
+++ lib/blurhash_image_page.dart
@@ -7,14 +7,12 @@ class BlurHashImagePage extends StatelessWidget {
   BlurHashImagePage({super.key});
 
   final List<String> _blurHashList = [
-    "LEHV6nWB2yk8pyoJadR*.7kCMdnj",
     "LGF5]+Yk^6#M@-5c,1J5@[or[Q6.",
     "L6Pj0^i_.AyE_3t7t7R**0o#DgR4",
     "LKO2?U%2Tw=w]~RBVZRi};RPxuwH"
   ];
 
   final List<String> _imageList = [
-    "https://blurha.sh/12c2aca29ea896a628be.jpg",
     "https://blurha.sh/e08adedc9f82ade9f9d5.jpg",
     "https://blurha.sh/a355ab362a07a267e078.jpg",
     "https://blurha.sh/ea9e499f8080ce9956a8.jpg",
